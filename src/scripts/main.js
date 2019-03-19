83c1121f57f63e4d91e2e4813eb6972a78244ce0
Clifton Matuszewski <cliftonmatuszewski@gmail.com>
2019-03-19 15:04:12 -0500

    Finished manufacturing excercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,65 @@ console.log(businesses);
 const outEl = document.querySelector("#output")
 outEl.innerHTML = "<h1>Active Businesses</h1>"
 
-let zipCodeProperty = "addressZipCode"
+// let zipCodeProperty = "addressZipCode"
 
-businesses.forEach(business => {
+// businesses.forEach(business => {
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity}, ${business["addressStateCode"]}, ${business[addressZipCode]}
+//     </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
+
+// Array to contain all the New York businesses
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+
+    return inNewYork
+})
+
+// console.log(newYorkBusinesses)
+
+// newYorkBusinesses.forEach(business => {
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity}, ${business["addressStateCode"]}, ${business['addressZipCode']}
+//     </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
+
+const manufacturingBusinesses = businesses.filter(business => {
+    let manufacturer = false
+
+    if (business.companyIndustry === "Manufacturing") {
+        manufacturer = true
+    }
+
+    return manufacturer
+})
+
+manufacturingBusinesses.forEach(business => {
   outEl.innerHTML += `
     <h2>${business.companyName}</h2>
     <section>
       ${business.addressFullStreet}
     </section>
     <section>
-    ${business.addressCity}, ${business["addressStateCode"]}, ${business[addressZipCode]}
+    ${business.addressCity}, ${business["addressStateCode"]}, ${business['addressZipCode']}
     </section>
   `
   outEl.innerHTML += "<hr/>"
